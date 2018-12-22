c2b3210135a1fc4ff997f07331df2b3e02e337fc
Niklas Kiefer <niklas.kiefer@camunda.com>
2018-12-22 12:17:46 +0100

    fix(project): fix linting errors

--- gulpfile.js
+++ gulpfile.js
@@ -27,61 +27,81 @@ const configuration = {
 };
 
 gulp.task('clean', function () {
+
   return del([
     './dist/*',
     '!./dist/.gitignore'
   ]);
+
 });
 
 gulp.task('html', function () {
+
   return gulp.src(configuration.paths.src.html)
     .pipe(rename('index.html'))
     .pipe(gulp.dest(configuration.paths.dist))
     .pipe(connect.reload());
+
 });
 
 gulp.task('css', function () {
+
   return gulp.src(configuration.paths.src.css)
     .pipe(concat('mrflap.dev.css'))
     .pipe(gulp.dest(configuration.paths.dist))
     .pipe(connect.reload());
+
 });
 
 gulp.task('js', function () {
+
   return gulp.src(configuration.paths.src.js)
     .pipe(concat('mrflap.dev.js'))
     .pipe(gulp.dest(configuration.paths.dist))
     .pipe(connect.reload());
+
 });
 
 gulp.task('vendor', function () {
+
   return gulp.src(configuration.paths.src.vendor)
     .pipe(gulp.dest(`${configuration.paths.dist}/vendor`));
+
 });
 
 gulp.task('connect', function () {
+
   return connect.server({
     root: configuration.paths.dist,
     port: configuration.localServer.port,
     livereload: true
   });
+
 });
 
 gulp.task('open', function () {
+
   return gulp.src(`${configuration.paths.dist}/index.html`)
     .pipe(open({ uri: configuration.localServer.url }));
+
 });
 
 gulp.task('watchCss', function () {
+
   return gulp.watch(configuration.paths.src.css, gulp.series(['css']));
+
 });
 
 gulp.task('watchHtml', function () {
+
   return gulp.watch(configuration.paths.src.html, gulp.series(['html']));
+
 });
 
 gulp.task('watchJs', function () {
+
   return gulp.watch(configuration.paths.src.js, gulp.series(['js']));
+
 });
 
 gulp.task('watch', gulp.parallel(['watchCss', 'watchHtml', 'watchJs']));

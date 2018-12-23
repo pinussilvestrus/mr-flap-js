4cff4e79816983324aa46bc514f2437e299cc6ad
Niklas Kiefer <niklas.kiefer@camunda.com>
2018-12-23 16:52:51 +0100

    chore(test): configure test runner

--- test/config/karma.unit.js
+++ test/config/karma.unit.js
@@ -5,58 +5,51 @@ module.exports = function (config) {
 
   config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '../..',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
-
-    // list of files / patterns to load in the browser
+    frameworks: ['mocha', 'chai'],
     files: [
-      'test/config/test-main.js',
-      { pattern: 'node_modules/chai/chai.js', included: false },
-      { pattern: 'test/**/*Spec.js', included: false }
+      'lib/**/*.js',
+      'lib/**/*.css',
+      'test/**/*.js'
     ],
-
-    // list of files / patterns to exclude
     exclude: [
     ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'lib/**/*.js': ['babel']
     },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
-    // web server port
+    reporters: ['spec'],
+    specReporter: {
+      maxLogLines: 5,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: true,
+      showSpecTiming: false
+    },
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['PhantomJS'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
+    concurrency: Infinity,
+    babelPreprocessor: {
+      options: {
+        presets: ['env'],
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
+        return file.originalPath.replace(/\.js$/, '.es5.js');
+      
+      },
+      sourceFileName: function (file) {
+
+        return file.originalPath;
+      
+      }
+    }
+
   });
 
 };

42982dc3dde9bfbe007839800b7f695aa40dcb28
Niklas Kiefer <niklas.kiefer@camunda.com>
2018-12-23 23:39:26 +0100

    test(canvas): add test coverage

---
+++ test/canvas/canvasSpec.js
@@ -0,0 +1,52 @@
+/* global it, describe, expect, beforeEach,  __html__, Canvas */
+/* eslint-disable no-unused-expressions */
+describe('Canvas', function () {
+    
+  var mrflapDiv;
+  
+  beforeEach(function () {
+  
+    document.body.innerHTML = __html__['index.html'];
+    mrflapDiv = $('.mrflap-playground');
+    
+  });
+  
+  it('#constructor', function () {
+    
+    // given
+    var canvas = new Canvas({
+      mrflapDiv: mrflapDiv
+    });
+  
+    // then
+    expect(canvas).not.to.be.undefined;
+    expect(canvas.mrflapDiv).to.equal(mrflapDiv);
+    expect(canvas.shapes.length).to.equal(0);
+        
+  });
+
+  it('#drawBird', function () {
+
+    const src = './dist/vendor/bird.png';
+
+    // given
+    var canvas = new Canvas({
+      mrflapDiv: mrflapDiv
+    });
+
+    // when
+    var bird = canvas.drawBird({
+      src: src
+    });
+
+    // then
+    expect(bird).not.to.be.undefined;
+    expect(bird.src).to.equal(src);
+
+    expect(canvas.shapes.length).to.equal(1);
+    expect(canvas.shapes[0].id).to.equal('shape-0');
+    expect(canvas.shapes[0].shape).to.eql(bird);
+  
+  });
+      
+});

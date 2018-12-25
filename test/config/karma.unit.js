89670e1ebeb20520d405682c70b86e310b36a13b
Niklas Kiefer <niklas.kiefer@camunda.com>
2018-12-25 16:30:00 +0100

    fix(karma): fix lint errors

--- test/config/karma.unit.js
+++ test/config/karma.unit.js
@@ -1,67 +1,67 @@
 // Karma configuration
 // Generated on Thu Dec 20 2018 19:22:16 GMT+0100 (Central European Standard Time)
 
-module.exports = function(config) {
+module.exports = function (config) {
 
-    config.set({
+  config.set({
 
-        basePath: '../..',
-        frameworks: ['mocha', 'chai'],
-        files: [
-            'dist/*',
-            'test/**/*.js',
-            'lib/**/*.js'
-        ],
-        exclude: [
-            'lib/jquery/*',
-            'lib/main/*'
-        ],
-        preprocessors: {
-            'dist/**/*.js': ['babel'],
-            'test/**/*.js': ['babel'],
-            'dist/*.html': ['html2js'],
-            'lib/**/*.js': ['coverage']
-        },
-        reporters: ['spec', 'coverage'],
-        coverageReporter: {
-            type: 'lcov',
-            dir: 'coverage/'
-        },
-        specReporter: {
-            maxLogLines: 5,
-            suppressErrorSummary: true,
-            suppressFailed: false,
-            suppressPassed: false,
-            suppressSkipped: true,
-            showSpecTiming: false
-        },
-        port: 9876,
-        colors: true,
-        logLevel: config.LOG_INFO,
-        autoWatch: false,
-        browsers: ['Chrome'],
-        singleRun: true,
-        concurrency: Infinity,
-        babelPreprocessor: {
-            options: {
-                presets: ['es2015'],
-                sourceMap: 'inline'
-            },
-            filename: function(file) {
+    basePath: '../..',
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'dist/*',
+      'test/**/*.js',
+      'lib/**/*.js'
+    ],
+    exclude: [
+      'lib/jquery/*',
+      'lib/main/*'
+    ],
+    preprocessors: {
+      'dist/**/*.js': ['babel'],
+      'test/**/*.js': ['babel'],
+      'dist/*.html': ['html2js'],
+      'lib/**/*.js': ['coverage']
+    },
+    reporters: ['spec', 'coverage'],
+    coverageReporter: {
+      type: 'lcov',
+      dir: 'coverage/'
+    },
+    specReporter: {
+      maxLogLines: 5,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: true,
+      showSpecTiming: false
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity,
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: 'inline'
+      },
+      filename: function (file) {
 
-                return file.originalPath.replace(/\.js$/, '.es5.js');
+        return file.originalPath.replace(/\.js$/, '.es5.js');
 
-            },
-            sourceFileName: function(file) {
+      },
+      sourceFileName: function (file) {
 
-                return file.originalPath;
+        return file.originalPath;
 
-            }
-        },
-        html2JsPreprocessor: {
-            stripPrefix: 'dist/'
-        }
+      }
+    },
+    html2JsPreprocessor: {
+      stripPrefix: 'dist/'
+    }
 
-    });
+  });
 
 };
